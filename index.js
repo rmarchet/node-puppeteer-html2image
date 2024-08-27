cf2006c087e2fbc2b87fbd3c886e943d0ad4b194
Roberto Marchetti <marchetti@beefree.io>
2024-08-27 22:48:23 +0200

    feat: updated puppeteer

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-
 const puppeteer = require('puppeteer')
-const devices = require('puppeteer/DeviceDescriptors')
 
 const args = process.argv.slice(2)
 if (args.length !== 2) {
@@ -14,12 +12,23 @@ const OUTFILE = args[1]
 
 const sourcepath = `file://${HTML}`
 
+const detectOS = (os) => {
+  const platform = process.platform
+  return platform === os
+}
+
+const launchOptions = {
+  headless: true,
+  args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  ...(detectOS('darwin') ? { executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' } : {})
+}
+
 async function generatePdf(err, data) {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  //visit the page and wait till all asset & XHR calls are done.
+  // visit the page and wait till all asset & XHR calls are done.
   try {
-    await page.goto(sourcepath, {waitUntil: 'networkidle2'})
+    await page.goto(sourcepath, { waitUntil: 'networkidle2' })
     await page.setViewport({
       width: 2000,
       height: 4000,
@@ -46,9 +55,8 @@ async function generatePng(err, data) {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   try {
-    //await page.emulate(devices['iPhone 6'])
     await page.goto(sourcepath)
-    await page.screenshot({path: OUTFILE, fullPage: true})
+    await page.screenshot({ path: OUTFILE, fullPage: true })
     await browser.close()
     await console.log("[ DONE ]")
   } catch (e) {
